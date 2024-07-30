8eaf33393559518d00f17b9f43aa2770f5076824
koipish <koipish@gmail.com>
2024-07-30 23:14:25 +0300

    added validation

--- tasks/validators.py
+++ tasks/validators.py
@@ -8,8 +8,9 @@ class TitleValidator:
         self.field = field
 
     def __call__(self, value):
-        reg = re.compile('^[а-яА-Яa-zA-Z0-9\.\-\,\№]')
+        reg = re.compile("^[а-яА-Яa-zA-Z0-9\.\-\,\№]")
         val = dict(value).get(self.field)
         if not bool(reg.match(val)):
-            raise ValidationError(f'{self.field} может содержать только буквы, цифры, точки, дефисы, запятые и пробелы.')
-
+            raise ValidationError(
+                f"{self.field} может содержать только буквы, цифры, точки, дефисы, запятые и пробелы."
+            )
